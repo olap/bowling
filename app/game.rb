d7e430c202ea6320f065ff45856c93d2f69658d4
Andrew <olap84@gmail.com>
2014-05-04 15:33:51 +0100

    First exceptional naive scorer. Added test cases suggested and passing the no halves or strikes test.

--- app/game.rb
+++ app/game.rb
@@ -1,10 +1,18 @@
 class Game
-  def add(pins)
+  def initialize
+    @frames = []
+  end
+
+  def roll(pins)
+    # defined only to take integers
+    @frames << pins
   end
 
   def score
+    @frames.reduce(:+)
   end
 
   def to_s
+    @frames.flatten.map(&:to_s).join
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,56 @@
 require './spec/spec_helper'
 
 describe Game do
-  describe 'to_s' do
-    let(:game) { Game.new }
-    it 'returns nothing yet' do
-      game.to_s.should eq('')
+  let(:game) { Game.new }
+
+  context 'a new game' do
+    describe 'to_s' do
+      it 'returns nothing yet' do
+        game.to_s.should eq('')
+      end
+    end
+  end
+
+  context 'a game of strikes' do
+    before do
+      12.times do
+        game.roll(10)
+      end
+    end
+
+    describe 'score' do
+      it 'scores 300' do
+        game.score.should eq(300)
+      end
+    end
+  end
+
+  context 'a game of no strikes or halves' do
+    before do
+      10.times do
+        game.roll(9)
+        game.roll(0)
+      end
+    end
+
+    describe 'score' do
+      it 'scores 90' do
+        game.score.should eq(90)
+      end
+    end
+  end
+
+  context 'a game of halves' do
+    before do
+      21.times do
+        game.roll(5)
+      end
+    end
+
+    describe 'score' do
+      it 'scores 150' do
+        game.score.should eq(150)
+      end
     end
   end
 end
